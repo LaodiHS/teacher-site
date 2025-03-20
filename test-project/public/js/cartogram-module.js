31cdd29f27bbaaf3ea909e8bb0740cf2c5e81731
LaodiHS <hasanseirafi69@gmail.com>
2025-03-20 17:11:45 +0000

    update map hanging test postions

--- test-project/public/js/cartogram-module.js
+++ test-project/public/js/cartogram-module.js
@@ -78,8 +78,8 @@ export function createCartogram(container) {
         if (province) {
           // Display the classical works
           svg.append("text")
-            .attr("x", width / 4)
-            .attr("y", 10 ) // Adjust this value to ensure visibility
+            .attr("x", width / 6)
+            .attr("y", 100 ) // Adjust this value to ensure visibility
             .attr("dominant-baseline", "hanging")
             .attr("font-size", "16px")
             .attr("fill", "#333")
