8f5e5377601204dc4ee2c847927d80b26598aa9f
LaodiHS <hasanseirafi69@gmail.com>
2025-05-25 11:26:10 -0700

    update hidden section

--- test-project/public/js/components/video-grid.js
+++ test-project/public/js/components/video-grid.js
@@ -3,6 +3,7 @@ class VideoGrid extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+    
     this.shadowRoot.innerHTML = `
       <style>
         :host {
